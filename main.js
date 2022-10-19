c3a65a1ed0fcab5dd4e11fa7f52e91c3288b070d
hironobu0824 <banu.asika0824.nobu@gmail.com>
2022-10-19 20:17:39 +0900

    セクション3からセクション4

---
+++ main.js
@@ -0,0 +1,14 @@
+function add(a, b) {
+    return a + b;
+}
+
+console.dir(add);
+ƒ add(a, b)
+    arguments: null
+    caller: null
+    length: 2
+    name: "add"
+    prototype: {constructor: ƒ}
+    [[FunctionLocation]]: main.js:1
+    [[Prototype]]: ƒ ()
+    [[Scopes]]: Scopes[1]
